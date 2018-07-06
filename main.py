b99762d7ae013894489ebd592ddb8aea2c2c9e6d
tc.hung <hungtc@solab.me.ntu.edu.tw>
2018-07-06 10:29:14 +0800

    Add argument parser

--- main.py
+++ main.py
@@ -2,52 +2,42 @@
 
 import numpy as np
 import cv2
+from argparse import ArgumentParser
 from time import time, sleep
 from subprocess import call
 import random
 
+parser = ArgumentParser()
+parser.add_argument('-d', '--device', help='device number: /dev/video#', dest='device', default=0)
+
+args = parser.parse_args()
+
+
 codec = 0x47504A4D # MJPG
 #codec = 844715353.0 # YUY2
 
-cam = cv2.VideoCapture(1)
-cam.set(cv2.CAP_PROP_FOURCC, codec)
-cam.set(cv2.CAP_PROP_FRAME_WIDTH,1920.0)
-cam.set(cv2.CAP_PROP_FRAME_HEIGHT,1080.0)
+cam = cv2.VideoCapture(args.device)
+cam.set(cv2.cv.CV_CAP_PROP_FOURCC, codec)
+cam.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH,1920.0)
+cam.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT,1080.0)
 
 init_time = time()
-captured = time()
-low_latency = False
+last = time()
 
-i = 1
 while True:
 
     # Capture frame-by-frame
     ret, frame = cam.read()
 
+    # Calculate fps
+    now = time()
+    fps = 1/(now -last)
+    last = time()
+    print '{:.2f}: {} x {} @ {:5.1f}'.format(now, frame.shape[1], frame.shape[0], fps)
+
     # Show the resulting frame
     cv2.imshow('frame', frame)
     cv2.waitKey(1)
-
-    # Calculate fps
-    fps = 1/(time() -captured)
-    captured = time()
-    print fps
-    print frame.shape
-
-    #if time()-init_time > 1 and not low_latency:
-    #    low_latency = not low_latency
-    #    call(['mwcap-control', '--video-output-lowlatency', 'on', '0:0'])
-    #    # cam.set(cv2.CAP_PROP_FPS,30.0)
-
-
-    # Write the resulting frame
-    # if time()-init_time > 3 and i <= 10:
-    #     if random.random()<0.2:
-    #         print str(i) + ": " + str(captured)
-    #         cv2.imwrite(str(i) + '.png', frame)
-    #         i += 1
-    # elif i > 10:
-    #     break
 
 # When everything done, release the capture
 cam.release()
